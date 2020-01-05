f773764c99be4d55af6b76602c7ec6829b5d8c41
AlexanderChirkin <chalex2000@yandex.ru>
2020-01-05 14:05:55 +0300

    Добавил на страницу с фильмами сегодняшние сеансы.

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -4,6 +4,19 @@ class FilmsController < ApplicationController
   end
 
   def show
-    @films = Film.all #Film.find(params[:id])
+    films = Array.new
+    Film.all.each do
+      |f|
+      t = f.as_json
+      t[:sessions] = Array.new
+      f.sessions.where("date_time > ? AND date_time < ?", Date.today.beginning_of_day, Date.today.end_of_day).each do
+        |s|
+        t[:sessions].append s.date_time.to_time.to_formatted_s(:time)
+      end
+      t[:sessions] = t[:sessions].sort
+      films.push(t)
+    end
+    @films = films
   end
+
 end
